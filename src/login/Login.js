b3fdd617098d7395967d911f2b51a0d2da1dc163
Murat Çakıroğlu <mcakiroglu.techproed@gmail.com>
2021-07-10 21:14:38 +0300

    v8

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validationSchema={Yup.object({
+          username: Yup.string()
+            .max(15, "Must be 15 characters or less")
+            .required("username Required"),
+          password: Yup.string()
+            .max(20, "Must be 20 characters or less")
+            .min(8, "Must be at least 8 character")
+            .required("Required"),
+        })}
+        onSubmit={(values, actions) => {
+          servis.login(values).then((res) => {
+            if (res.status === 200) {
+              const userInfo = res.data;
+            }
+          });
+          if (userInfo && idAdmin) {
+            history.push("/admin");
+          } else {
+            history.push("/user");
+          }
+        }}
+      ></Formik>
+    </div>
+  );
 };
 
 export default Login;
