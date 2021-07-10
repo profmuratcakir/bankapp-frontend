c3eb0c8414763e33940f5bdff3f7b9e785c0f66e
Murat Çakıroğlu <mcakiroglu.techproed@gmail.com>
2021-07-10 22:21:43 +0300

    v9

--- src/login/Login.js
+++ src/login/Login.js
@@ -2,17 +2,42 @@ import React from "react";
 import { Formik, Field, Form } from "formik";
 import * as Yup from "yup";
 import { toast, ToastContainer } from "react-toastify";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import { Button } from "@material-ui/core";
+import { TextField } from "formik-material-ui";
+import "./Login.css";
 
 const LoginForm = (props) => (
-  <Container>
-    <Form>
-      <label htmlFor="username">Usere Name</label>
-      <Field name="username" type="text" />
-      <label htmlFor="password">Password</label>
-      <Field name="password" type="password" />
-      <button type="submit">Submit</button>
-    </Form>
+  <Container className="d-flex justify-content-center">
+    <fieldset>
+      <legend>Login</legend>
+      <Form>
+        <Row className="justify-content-center">
+          <Col xs={12} md={6} className="text-center">
+            <label htmlFor="username">Usere Name</label>
+            <Field component={TextField} name="username" type="text" />
+          </Col>
+
+          <Col xs={12} md={6} className="text-center">
+            <label htmlFor="password">Password</label>
+            <Field component={TextField} name="password" type="password" />
+          </Col>
+        </Row>
+        <Row className="mt-4 ">
+          <Col className="d-flex justify-content-center">
+            <Button
+              type="submit"
+              onClick={props.submitForm}
+              disabled={props.isSubmitting}
+              variant="contained"
+              color="secondary"
+            >
+              Submit
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </fieldset>
   </Container>
 );
 
