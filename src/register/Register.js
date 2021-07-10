d855f5978e9e1238cf4bb1a3e64f9d61bb87afb3
Murat Çakıroğlu <mcakiroglu.techproed@gmail.com>
2021-07-10 23:12:32 +0300

    v11

--- src/register/Register.js
+++ src/register/Register.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Formik } from "formik";
 
 const Register = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastname: "",
+          dob: "",
+          email: "",
+          username: "",
+          role: ["user"],
+          password: "",
+          confirmPassword: "",
+        }}
+        validationSchema={RegisterSchema}
+        onSubmit={(values, actions) => {
+          servis.register(values).then((res) => {
+            if (res.status === 200) {
+              toast.success("Register Successful", {
+                position: toast.POSITION.TOP_CENTER,
+              });
+              actions.resetForm();
+            } else {
+              toast.error(res.data.message, {
+                position: toast.POSITION.TOP_CENTER,
+              });
+            }
+            actions.setSubmitting(false);
+          });
+        }}
+        component={RegistrationForm}
+      ></Formik>
+    </div>
+  );
 };
 
 export default Register;
